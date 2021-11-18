817d9bda20bc95c58073e7cce26ba8d64a5feaa1
dcompane <dcompane@gmail.com>
2021-11-17 18:55:08 -0800

    Update aapi_upgrade.ps1

--- aapi_upgrade.ps1
+++ aapi_upgrade.ps1
@@ -1,13 +1,38 @@
 #! /bin/pwsh
+
+# MIT License
+# Copyright (c) 2021 Daniel Companeetz, BMC Software, Inc.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# SPDX-License-Identifier: MIT
+# For information on SDPX, https://spdx.org/licenses/MIT.html
+
 param ([int] $myversion=9, [int] $myrelease=20, [int] $fixpack)
 #NOTE: Do not forget to change the versions
-# This is to run as see if the user isLocalAdmin
+
 # Comment next line if no debug information is required
 Set-PSDebug -Trace 1
 
 #From <https://www.red-gate.com/simple-talk/sysadmin/powershell/how-to-use-parameters-in-powershell/>
 #param ([int] $myversion=9, [int] $myrelease=20, [int] $fixpack)
 
+# This is to run as see if the user isLocalAdmin
 If($isWindows) {
     $outpath = $env:TEMP+"\padev_current.exe"
     $thisOS = "Windows"
@@ -20,6 +45,7 @@ If($isWindows) {
     }
 }
 
+#No validation if Linux. Assumes it is the EM User
 If($isLinux) {
     $outpath = "/tmp/padev_current.exe"
     $thisOS = "Unix"
@@ -42,10 +68,6 @@ if ($fixpack -eq $null) {
     write-host "Starting installation of $outpath"
     Start-Process -Wait -Filepath "$outpath" -ArgumentList "-s -v"
     write-host "Completed installation of $outpath"
-    <# comments
-        multilines
-        # Start-Process -Filepath "$outpath" -ArgumentList "-s -v" -wait
-    #>
 }
 # Debuggone
 Set-PSDebug -Trace 0
